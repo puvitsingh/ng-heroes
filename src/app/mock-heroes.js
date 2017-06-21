7cd5a906cbf0f4dc40faf892cdd508e5b93318fd
Puvit <puvit.singh@gmail.com>
2017-06-21 01:28:05 -0400

    commit

---
+++ src/app/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Mr. Nice', age: 21 },
+    { id: 12, name: 'Narco', age: 21 },
+    { id: 13, name: 'Bombasto', age: 21 },
+    { id: 14, name: 'Celeritas', age: 21 },
+    { id: 15, name: 'Magneta', age: 21 },
+    { id: 16, name: 'RubberMan', age: 21 },
+    { id: 17, name: 'Dynama', age: 21 },
+    { id: 18, name: 'Dr IQ', age: 21 },
+    { id: 19, name: 'Magma', age: 21 },
+    { id: 20, name: 'Tornado', age: 21 }
+];
+//# sourceMappingURL=mock-heroes.js.map
