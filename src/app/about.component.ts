5fa26b83d9c3f8146f4f54986dc681fa7adbb903
n0212741 <puvit.singh@libertymutual.com>
2017-06-27 16:19:09 -0400

    minor change to about.component.ts

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-about',
-  template: '<h3>About</h3>'
+  template: '<h3>About something</h3>'
 })
 export class AboutComponent { }
